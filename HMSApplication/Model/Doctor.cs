db1051d4ca8987c66de62849bd4ae6f6fa795e0d
usamameer256@gmail.com <usamameer256@gmail.com>
2024-09-11 15:10:54 -0700

    models

---
+++ HMSApplication/Model/Doctor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMSApplication.Model
+{
+    public class Doctor
+    {
+        public int DoctorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Specialty { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public decimal Fee { get; set; }
+    }
+}
